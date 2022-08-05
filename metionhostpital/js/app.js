bd17cc77afb11a5a167435b66f005e0a5b648507
phyothihakyaw <kyawphyothiha7@gmail.com>
2022-08-05 08:29:50 +0630

    8/5/22

---
+++ metionhostpital/js/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	// console.log('i m working')
+
+	// Start Header Section
+
+// Start Banner Section
+
+// $(".carousel").carousel({
+// 	// interval:5000
+})
+
+// End Banner Section
+
+	// End Header Section
+// })
+
+// Start Info Section
+	$(window).scroll(function(){
+		let getscrollpoint = $(this).scrollTop();
+		// console.log(getscrollpoint);
+
+		if(getscrollpoint >=220 ){
+			$(".infotexts").addClass("fromlefts");
+			$(".infopics").addClass("fromrights");
+
+		}else{
+			$(".infotexts").removeClass("fromlefts");
+			$(".infopics").removeClass("fromrights");
+		}
+	});
+	// End Info Section
