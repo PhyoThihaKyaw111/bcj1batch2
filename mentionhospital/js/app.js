f5b8e50df3de1dedffa59c90e9f713b882d5a33f
phyothihakyaw <kyawphyothiha7@gmail.com>
2022-08-05 14:36:03 +0630

    project

--- mentionhospital/js/app.js
+++ mentionhospital/js/app.js
@@ -1,75 +1,134 @@
 $(document).ready(function(){
+    // console.log('hi');
 
-    // Start Banner Section 
-    $(".carousel").carousel({
-        interval:2000
-    });
-    // End Banner Section 
+    // Start Header Section
+    //Start Banner Section
 
+    $('.carousel').carousel({
+        interval:1000
+    })
 
-    // Start Info SEction
+    //End Banner Section
+
+    // End Header Section
+
+    // start info section
     $(window).scroll(function(){
         let getscrollpoint = $(this).scrollTop();
-
         // console.log(getscrollpoint);
-
-        if( getscrollpoint >= 420){
-            $(".infotexts").addClass("fromlefts");
-            $(".infopics").addClass("fromrights");
+        
+        if(getscrollpoint >= 220){
+            $('.infotexts').addClass("fromlefts");
+            $('.infopics').addClass("fromrights");
         }else{
-            $(".infotexts").removeClass("fromlefts");
-            $(".infopics").removeClass("fromrights");
+            $('.infotexts').removeClass("fromlefts");
+            $('.infopics').removeClass("fromrights");
         }
-    })
-    // End Info SEction
-
-    // Start Premises SEction
-
-    $("#lightslider").lightSlider({
-        item:4,
-        auto:true,
-        loop:true,
-        slideMove:1,
-        speed:600
     });
-    
-    // End Premises SEction
+    // End info section
 
+     // Start ADV Section 
+     $("#videos").click(function(){
+        var getmodal = $(this).data("bs-target");
+        var getvideosrc = $(this).data("video");
+        var videourlwithauto = getvideosrc +"?autoplay=1";
 
-    // Start Pricing SEction
+        $(getmodal + " iframe").attr("src",videourlwithauto);   
+
+        $(getmodal + " button.btn-close").click(function(){
+            $(getmodal + " iframe").attr("src",getvideosrc);
+        });
+
+        $(getmodal).click("hidden.bs.modal",function(){
+            $(getmodal + " iframe").attr("src",getvideosrc);
+        });
+    })
+    // End ADV Section 
+
+    //Start Premises Section
+
+    //Start lightslider
+    $('#lightslider').lightSlider({
+        // auto:true,   //auto:true;ko ma use chin yin .play() use ml
+        loop:true,
+        item:4,       
+        slideMove:1,
+        speed:600,
+    }).play();
+    // End lightslider
+    //End Premises Section
+        
+    // Start Pricing Section
 
     $(window).scroll(function(){
+        let getscroll = $(this).scrollTop();
+        // console.log(getscroll);
 
-        var getscroll = $(this).scrollTop();
-        // console.log(getscroll); 
-
-        if (getscroll >= 2500){
+        if(getscroll >= 2450){
             $(".cardones").addClass("movelefts");
-            $(".cardtwos").addClass("movebottoms")
-            $(".cardthrees").addClass("moverights")
+            $(".cardtwos").addClass("movebottoms");
+            $(".cardthrees").addClass("moverights");
         }else{
             $(".cardones").removeClass("movelefts");
-            $(".cardtwos").removeClass("movebottoms")
-            $(".cardthrees").removeClass("moverights")
+            $(".cardtwos").removeClass("movebottoms");
+            $(".cardthrees").removeClass("moverights");
         }
-    })
+    });
 
-    // End Pricing SEction
+    //End Pricing Section
 
+    // Start Join Us Section
+    $('#accordion').accordion();
+    // End Join Us Section
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // Start Footer Section
+    $('#getyear').text(new Date().getUTCFullYear());
     
-})
+    // End Footer Section
+
+    // Start Progress 
+    $(window).scroll(function(){
+        var getprogress = $("#progresses"); 
+        var getprogressval = $("#progressvalues");
+        var getscrolltop = $(this).scrollTop();
+        // console.log(getscrolltop);
+
+// ----------------------------------------------------------------
+    // By Jquery
+
+        // // st * 100 / (pj height - cvheight)
+    
+        // var getscrollheight = $(document).height();
+        // console.log(getscrollheight);
+        // var getclientheight = $(window).height();
+        // console.log(getclientheight);
+         
+        // var calcheight = getscrollheight - getclientheight;
+        // var getfinalheight = Math.round(getscrolltop * 100 / calcheight)
+
+        // getprogressval.text(`${getfinalheight}%`)
+
+        // getprogress.css({
+        //     "background":`conic-gradient(steelblue ${getfinalheight}%,#eee ${getfinalheight}%)`
+        // })
+// ----------------------------------------------------------------
+    // By Javascript 
+
+        var getscrollheight = document.documentElement.scrollHeight;
+        // console.log(getscrollheight);
+        var getclientheight = document.documentElement.clientHeight;
+        // console.log(getclientheight);
+        var calcheight = getscrollheight - getclientheight;
+        var getfinalheight = Math.round(getscrolltop * 100 / calcheight);
+        // console.log(getfinalheight);
+
+        getprogressval.text(`${getfinalheight}%`);
+        getprogress.css({
+            "background":`conic-gradient(steelblue ${getfinalheight}%,#eee ${getfinalheight}%)`
+        });
+
+    });
+    // End Progress
+
+
+});
